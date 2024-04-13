21fb1d1fe5680084a2afd8a4dff106e414341dd2
layanat <tounsilayana@gmail.com>
2024-04-12 23:19:40 -0400

    Added Landing Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Checkin from './pages/Checkin'; // Adjust the import path as necessary
+import LandingPage from './pages/LandingPage';
 import Stats from './pages/Stats';
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
       </Routes>
       <Routes>
         <Route path="/stats" element={<Stats />} />
+       
+      </Routes>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
        
       </Routes>
       </BrowserRouter>
