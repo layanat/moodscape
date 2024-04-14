2befa0e6f4175857662424d9651977bb4a48ff42
layanat <tounsilayana@gmail.com>
2024-04-14 14:25:54 -0400

    added confirmation page

---
+++ src/pages/CheckinConfirmation.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styling/CheckinConfirmation.css"; // Path to your CSS file for styling
+
+const CheckinConfirmation = () => {
+  return (
+    <div className="confirmation-container">
+      <h2>Your entry for today was submitted</h2>
+      <div className="check-mark-circle">✔️</div>
+      <Link to="/" className="home-button">
+        Return to Home Page
+      </Link>
+    </div>
+  );
+};
+
+export default CheckinConfirmation;
